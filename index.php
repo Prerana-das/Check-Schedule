2f7aed9cec03c361a659432910769bd97ba9721e
Prerana Das <32504893+Prerana-das@users.noreply.github.com>
2019-05-11 00:08:21 +0600

    Update index.php

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
 		</div>
 	</div>
 	  
-	  <!-- Master -->
+	  
 		
 	<script type="text/javascript" src="js/jquery-1.12.4.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
